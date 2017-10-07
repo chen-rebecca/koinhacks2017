08ebfffb76226cca2fc42c0a3d02e832aefa9091
Rebecca Chen <chen.rebecca@berkeley.edu>
2017-10-07 15:16:47 -0700

    basic working login

--- FoodBank/app/src/main/java/com/example/foodbank/foodbank/LoginActivity.java
+++ FoodBank/app/src/main/java/com/example/foodbank/foodbank/LoginActivity.java
@@ -3,25 +3,36 @@ package com.example.foodbank.foodbank;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.graphics.Color;
 import android.content.Intent;
 import android.view.View;
 import android.widget.*;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Button button;
+    Button login;
+    EditText username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
 
-        button=(Button)findViewById(R.id.loginbutton);
-        button.setOnClickListener(new View.OnClickListener() {
+        login = (Button) findViewById(R.id.loginbutton);
+        username = (EditText)findViewById(R.id.username);
+        password = (EditText)findViewById(R.id.password);
+
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),Home.class);
-                startActivity(i);
+                if (username.getText().toString().equals("admin") &&
+                        password.getText().toString().equals("admin")) {
+                    Intent i = new Intent(getApplicationContext(),Home.class);
+                    startActivity(i);
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "Wrong Credentials",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
